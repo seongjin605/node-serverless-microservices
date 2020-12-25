56de90bb02e31bb0bd31b607ec7520b20e395205
preciouStar <seongjin605@users.noreply.github.com>
2020-12-25 18:25:17 +0900

    Test api for http methods

--- routes/api/test.js
+++ routes/api/test.js
@@ -1,22 +1,11 @@
 const router = require('express').Router({ mergeParams: true });
 
-// function test() {
-//   res.send({
-//     name: 'test',
-//     email: 'test@amazon.com',
-//     address: 'seoul'
-//   });
-// }
-
 module.exports = (services, models) => {
   console.log('🚀 ~ file: test.js ~ line 12 ~ models', models);
   console.log('🚀 ~ file: test.js ~ line 12 ~ services', services);
-  router.post('/', function (req, res) {
-    res.send({
-      name: 'test',
-      email: 'test@amazon.com',
-      address: 'seoul'
-    });
+  router.post('/', (req, res, next) => {
+    res.status(200).send(['Jesus', 'I love to praise your name', 'My Story', 'Days of Elijah']);
+    console.log('next:', next);
   });
   return router;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,17 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-// router.get('/', function (req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
 
-// router.post('/', function (req, res) {
-//   res.send({
-//     name: 'test',
-//     email: 'test@amazon.com',
-//     address: 'seoul'
-//   });
-// });
+router.post('/api/test', (req, res) => {
+  res.send({
+    name: 'test',
+    email: 'test@amazon.com',
+    address: 'seoul'
+  });
+});
 
-// module.exports = router;
-
-const router = require('express').Router({ mergeParams: true });
-
-module.exports = (services, models) => {
-  router.use('/api/test', require('./api/test')(services, models));
-  return router;
-};
+module.exports = router;
