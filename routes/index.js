5bfa4792def74b7f20497e78aec7dec8a5f2db84
preciouStar <qkrtjdwls605@naver.com>
2021-04-26 22:41:20 +0900

    Add endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const app = require('express').Router({ mergeParams: true });
 const test = require('./api/test');
+const rabbit = require('./api/rabbit');
+
 app.use('/api/test', test);
+app.use('/api/rabbit', rabbit);
 
 module.exports = app;
