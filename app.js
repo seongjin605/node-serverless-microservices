2f3f4f30f368447acdce34ca3c79aa6769991a9d
preciouStar <seongjin605@users.noreply.github.com>
2021-05-02 21:19:40 +0900

    Add mysql and change directory

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./src/routes/index');
 // const usersRouter = require('./routes/users');
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
